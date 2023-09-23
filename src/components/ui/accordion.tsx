10496550aebde056d650bd3a19e0f8e8cc16a790
dev_panda <75636672+TohirBabs@users.noreply.github.com>
2023-09-24 00:07:34 +0100

    updates

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -46,7 +46,7 @@ const AccordionContent = React.forwardRef<
   <AccordionPrimitive.Content
     ref={ref}
     className={cn(
-      "overflow-hidden text-xs transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down",
+      "overflow-hidden text-xs text-left transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down",
       className
     )}
     {...props}
